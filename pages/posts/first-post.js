b0f72c8fc3cfe0d5918ccac248c51f72fa11e8d7
unknown <trex33160@gmail.com>
2021-05-18 23:20:08 +0545

    added metatags and css using layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,22 @@
-import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title> First Post </title>
+      </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
+        <Image
+          src="/images/profile.jpg"
+          height={144}
+          width={144}
+          alt={"My name"}
+        ></Image>
       </h2>
-    </>
+    </Layout>
   );
 }
